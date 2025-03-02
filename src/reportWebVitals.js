e47503ac771b75106bb9b6d78f4c67ee34f35ece
nagashree7 <nagashree721@gmail.com>
2025-03-02 20:36:47 +0530

    commit

---
+++ src/reportWebVitals.js
@@ -0,0 +1,8 @@
+// src/reportWebVitals.js
+
+const reportWebVitals = (metric) => {
+    console.log(metric); // You can replace this with your preferred analytics or reporting tool
+  };
+  
+  export default reportWebVitals;
+  
