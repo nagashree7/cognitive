689977250db47dcbecf7911594ff775bbfa83a3e
nagashree7 <149050136+nagashree7@users.noreply.github.com>
2025-03-03 12:34:06 +0530

    Delete src directory

--- src/reportWebVitals.js
+++
@@ -1,8 +0,0 @@
-// src/reportWebVitals.js
-
-const reportWebVitals = (metric) => {
-    console.log(metric); // You can replace this with your preferred analytics or reporting tool
-  };
-  
-  export default reportWebVitals;
-  
